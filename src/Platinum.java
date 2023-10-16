936bbb30f628cd037eb2f170d080abfb2fc40041
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-10-15 21:28:25 -0500

    Add Member and Methods

--- src/Platinum.java
+++ src/Platinum.java
@@ -1,4 +1,16 @@
 // Derived class
-public class Platinum {
-    
+public class Platinum extends Customer{
+    int bonusBucks;
+
+    public Platinum(String fn, String ln, String gi, double as, int bb){
+        super(fn, ln, gi, as);
+        bonusBucks = bb;
+    }
+
+    public int getBonusBucks(){
+        return bonusBucks;
+    }
+    public void setBonusBucks(int bb){
+        bonusBucks = bb;
+    }
 }
