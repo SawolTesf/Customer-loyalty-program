948c9a5613681b1e9f3eaa703047bd2c07f18220
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-10-15 21:25:54 -0500

    Add Member and Methods

--- src/Gold.java
+++ src/Gold.java
@@ -1,4 +1,16 @@
 // Derived Class
-public class Gold {
-    
+public class Gold extends Customer{
+    protected int discount;
+
+    Gold(String fn, String ln, String gi, double as, int d){
+        super(fn, ln, gi, as);
+        discount = d;
+    }
+
+    public int getDiscount(){
+        return discount;
+    }
+    public void setDiscount(int d){
+        discount = d;
+    }
 }
